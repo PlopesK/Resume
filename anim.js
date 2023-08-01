637950bbbc1ed3f740296e375d1206fd1869d658
PlopesK <popi081004@hotmail.com>
2023-08-01 19:58:59 -0300

    Start of function

---
+++ anim.js
@@ -0,0 +1,21 @@
+const savedTheme = localStorage.getItem("theme");
+let dark;
+function changeTheme() {
+  if (!dark) {
+    dark = true;
+    localStorage.setItem("theme", "dark");
+
+    document.documentElement.style.setProperty("--Page", "darkslategray");
+    document.documentElement.style.setProperty("--Text", "white");
+    document.documentElement.style.setProperty("--Dark", "rgb(228, 228, 228)");
+    document.documentElement.style.setProperty("--Link", "black");
+  } else {
+    dark = false;
+    localStorage.setItem("theme", "white");
+
+    document.documentElement.style.setProperty("--Page", "rgb(228, 228, 228)");
+    document.documentElement.style.setProperty("--Text", "black");
+    document.documentElement.style.setProperty("--Dark", "darkslategray");
+    document.documentElement.style.setProperty("--Link", "white");
+  }
+}
