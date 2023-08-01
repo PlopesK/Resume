406b047bacf02d95b253b9629f8a6f5314664eb2
PlopesK <popi081004@hotmail.com>
2023-08-01 20:31:42 -0300

    Functional dark mode :)

--- anim.js
+++ anim.js
@@ -6,14 +6,17 @@ function changeTheme() {
     localStorage.setItem("theme", "dark");
 
     document.documentElement.style.setProperty("--Page", "darkslategray");
+    document.documentElement.style.setProperty("--Section", "#111919");
     document.documentElement.style.setProperty("--Text", "white");
     document.documentElement.style.setProperty("--Dark", "rgb(228, 228, 228)");
     document.documentElement.style.setProperty("--Link", "black");
+    document.getElementById("icon");
   } else {
     dark = false;
     localStorage.setItem("theme", "white");
 
     document.documentElement.style.setProperty("--Page", "rgb(228, 228, 228)");
+    document.documentElement.style.setProperty("--Section", "white");
     document.documentElement.style.setProperty("--Text", "black");
     document.documentElement.style.setProperty("--Dark", "darkslategray");
     document.documentElement.style.setProperty("--Link", "white");
